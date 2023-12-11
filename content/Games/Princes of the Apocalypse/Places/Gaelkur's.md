9c7ef853b4af2d0990e4b2e9556033b1e7a7a83c
phillipmkey <pkey@pkey.us>
2023-12-11 15:32:18 -0600

    Quartz sync: Dec 11, 2023, 3:32 PM

---
+++ content/Games/Princes of the Apocalypse/Places/Gaelkur's.md
@@ -0,0 +1,13 @@
+---
+type: place
+faction: 
+location: Red Larch
+world: Princes of the Apocalypse
+date: 2023-12-11
+description: ""
+race: 
+gender: 
+class:
+---
+# [[Gaelkur's]]
+
---
+++ update.cmd
@@ -0,0 +1,4 @@
+@@ ECHO OFF
+xcopy "C:\Users\pkey\OneDrive\Documents\Obsidian\TTRPG Player Notes\*" C:\Users\pkey\Downloads\quartz\content\ /E/H/C/Y
+git commit -a -m "Automated Commit"
+npx quartz sync
