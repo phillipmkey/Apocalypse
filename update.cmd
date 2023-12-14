f0292c6394a9a25d65f3b334671342aa6f43f216
phillipmkey <pkey@pkey.us>
2023-12-14 08:51:50 -0600

    Automated Commit

--- update.cmd
+++ update.cmd
@@ -1,5 +1,4 @@
 @@ ECHO OFF
-REM xcopy "C:\Users\pkey\OneDrive\Documents\Obsidian\TTRPG Player Notes\*" C:\Users\pkey\Downloads\quartz\content\ /E/H/C/Y
 robocopy "C:\Users\pkey\OneDrive\Documents\Obsidian\TTRPG Player Notes\" "C:\Users\pkey\Downloads\quartz\content\" /MIR
 git commit -a -m "Automated Commit"
 npx quartz sync
